b228140efc4152c78be883253c337a90b5198ba1
Ksenija <ksenija.rasale@gmail.com>
2025-05-06 11:01:59 +0300

    Test change in auth.py

--- website/auth.py
+++ website/auth.py
@@ -76,7 +76,8 @@ def sign_up():
             return redirect(url_for('views.home'))
         except Exception as e:
             db.session.rollback()
-            flash(f"An error occurred: {str(e)}", category='error')
+            flash(f"An error occurred: {str(e)}", category='error')     # test change
+
 
     return render_template('sign_up.html', user=current_user)
 
